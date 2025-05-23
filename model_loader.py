3da741461fb7d4e436cdd4bcafecade6a04304ed
Rangga Priago <141876195+RPriago@users.noreply.github.com>
2025-05-23 23:19:13 +0700

    Delete model_loader.py

--- model_loader.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import numpy as np
-
-with open("model.p", "rb") as f:
-    model = pickle.load(f)
-
-def predict_gesture(features):
-    features = np.array(features).reshape(1, -1)
-    return model.predict(features)[0]
